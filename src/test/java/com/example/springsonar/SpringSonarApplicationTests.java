bef976eba3d2c6e40466a0bed9b77f9a24ab45b9
Ellazar Hinagpis <ellazarhinagpis@gmail.com>
2022-02-08 11:50:02 +0800

    INITIAL COMMIT
    (cherry picked from commit 2f8ad2b928a9413ea42d4cd074dc1c7b7432f281)

---
+++ src/test/java/com/example/springsonar/SpringSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSonarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
