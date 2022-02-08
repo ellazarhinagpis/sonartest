2f8ad2b928a9413ea42d4cd074dc1c7b7432f281
Ellazar Hinagpis <ellazarhinagpis@gmail.com>
2022-02-08 11:50:02 +0800

    INITIAL COMMIT

---
+++ src/main/java/com/example/springsonar/SpringSonarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSonarApplication.class, args);
+    }
+
+}
